f7d0285e350209afca239ce3d35ab020f7a2c0d0
yujiw001 <wany5d1401@gmail.com>
2019-10-28 11:06:03 -0700

    finished creating the slidesshow

---
+++ src/component/dropdown_box/dropdown_box.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './dropdown_box.styles.css';
+import {Link} from 'react-router-dom';
+
+const DropDownBox = () => (
+    <div className='dropdown-box'>
+        
+    </div>
+)
---
+++ src/component/slideshow/slideshow.component.jsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import slide_2 from '../../images/slide_2.jpg';
+import slide_3 from '../../images/slide_3.jpg';
+import slide_4 from '../../images/slide_4.jpg';
+import './slideshow.styles.css'
+const slideImages =[
+  slide_2,
+  slide_3,
+  slide_4
+];
+
+const properties = {
+  duration: 5000,
+  transitionDuration: 500,
+  infinite: true,
+  indicators: true,
+  arrows: true,
+  onChange: (oldIndex, newIndex) => {
+    console.log(`slide transition from ${oldIndex} to ${newIndex}`);
+  }
+}
+ 
+const Slideshow = () => {
+    return (
+      <div className="slide-container">
+        <Slide {...properties}>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[0]})`}}>
+              <span>Slide 1</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[1]})`}}>
+              <span>Slide 2</span>
+            </div>
+          </div>
+          <div className="each-slide">
+            <div style={{'backgroundImage': `url(${slideImages[2]})`}}>
+              <span>Slide 3</span>
+            </div>
+          </div>
+        </Slide>
+      </div>
+    )
+}
+
+export default Slideshow;
