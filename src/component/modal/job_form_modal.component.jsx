87c72e6b223871e386ca070ed583f5fc5a57d327
pengc1995 <pengc1995@gmail.com>
2019-11-27 13:59:37 -0800

    test

--- src/component/modal/job_form_modal.component.jsx
+++ src/component/modal/job_form_modal.component.jsx
@@ -1,161 +1,172 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Form, Input, Modal, Button } from 'antd';
+
+import { Modal, Button, Form, Input } from 'antd';
 
 import UploadButton from '../Upload/Upload';
 
-//import WrappedRegistrationForm from '../JoinUs/JobForm.component';
-import '../JoinUs/JobForm.component.style.css';
+import './JobForm.component.style.css';
+
+const CollectionCreateForm = Form.create( { name: 'form_in_modal'} )(
+  class extends Component {
+
+    constructor() {
+      super();
+      this.state = {
+        formLayout: 'vertical',
+        confirmDirty: false,
+      };
+    }
+
+    handleSubmit = e => {
+      e.preventDefault();
+      this.props.form.validateFieldsAndScroll((err, values) => {
+        if (!err) {
+          console.log('Received values of form: ', values);
+        }
+      });
+    };
+  
+    handleConfirmBlur = e => {
+      const { value } = e.target;
+      this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    };
+
+    render() {
+      const { formLayout } = this.state;
+      const { visible, onCancel, onCreate, form } = this.props;
+      const { getFieldDecorator } = form;
+      const { TextArea } = Input;
+      return (
+        <Modal
+          visible={visible}
+          width={600}
+          okText="Submit"
+          onOk={onCreate}
+          onCancel={onCancel}
+          closable={false}
+        >
+          <h1 className='fh_rec_det_apply_title'>{this.props.title}</h1>
+          <hr />
+          <h1 className='fh_rec_det_apply_city'>City: {this.props.area}</h1>
+
+          <Form layout={formLayout} onSubmit={this.handleSubmit} >
+          <div className=''>
+
+            <div className='fh_rec_det_apply_name'>
+              <Form.Item label='Name' >
+                {
+                  getFieldDecorator('firstname', {
+                    rules: [{ required: true, message: 'Please input your first name!', whitespace: true }],
+                  })
+                  (<Input placeholder='First Name' allowClear={true} style={{width:'250px'}}/>)
+                }
+              </Form.Item>
+              
+              <Form.Item label='' >
+                {
+                  getFieldDecorator('lastname', {
+                  rules: [{ required: true, message: 'Please input your last name!', whitespace: true }],
+                  })
+                  (
+                    <div className='fh_rec_det_apply_last_name'>
+                      <Input placeholder='Last Name' allowClear={true} style={{width:'250px'}}/>
+                    </div>
+                  )
+                }
+              </Form.Item>
+            </div>
+
+            <Form.Item label="E-mail" >
+              {getFieldDecorator('email', {
+                rules: [
+                  {
+                    type: 'email',
+                    message: 'The input is not valid E-mail!',
+                  },
+                  {
+                    required: true,
+                    message: 'Please input your E-mail!',
+                  },
+                ],
+              })(<Input placeholder='Email Address' allowClear={true} />)}
+            </Form.Item>
+
+            <Form.Item label='Description' >
+              <TextArea rows={4} allowClear={true} />
+            </Form.Item>
+
+            <Form.Item label='Upload Resume' required>
+              {/* {
+                getFieldDecorator('resume', {
+                  rules: [ {validator: this.handleUploadValidator}],
+                })
+                ( */}
+                  <UploadButton title='Add a File' />
+                {/* ) 
+              } */}
+            </Form.Item>
+          </div>
+        </Form>
+        </Modal>
+      
+      );
+    }
+  },
+);
+
 
 class JobApplication extends Component {
-
-  constructor() {
-    super();
-    this.state = {
-      loading: false,
-      visible: false,
-      formLayout: 'vertical',
-      confirmDirty: false,
-    };
-  }
-
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.form.validateFieldsAndScroll((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values);
-      }
-    });
-  };
-
-  handleConfirmBlur = e => {
-    const { value } = e.target;
-    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  state = {
+    visible: false,
   };
 
   showModal = () => {
-    this.setState({
-      visible: true,
-    });
-  };
-
-  handleOk = () => {
-    this.setState({ loading: true });
-    setTimeout(() => {
-      this.setState({ loading: false, visible: false });
-    }, 3000);
+    this.setState({ visible: true });
   };
 
   handleCancel = () => {
     this.setState({ visible: false });
   };
 
+  handleCreate = () => {
+    const { form } = this.formRef.props;
+    form.validateFields((err, values) => {
+      if (err) {
+        return;
+      }
+
+      console.log('Received values of form: ', values);
+      form.resetFields();
+      this.setState({ visible: false });
+    });
+  };
+
+  saveFormRef = formRef => {
+    this.formRef = formRef;
+  };
+
   render() {
 
-    const { visible } = this.state.visible;
-    const { loading } = this.state.loading;
-    const { formLayout } = this.state.formLayout;
-    const { getFieldDecorator } = this.props.form;
-    const { TextArea } = Input;
-    
+    let pos_title = this.props.title;
+    let pos_area = this.props.area;
+    /* .......... */
+
     return (
       <div>
         <Button className='fh_rec_det_apply_button' onClick={this.showModal}>
-            <span className='fh_rec_det_apply_content'>Apply for this position ></span>
+          <span className='fh_rec_det_apply_content'>Apply for this position ></span>
         </Button>
 
-        <Modal
-          visible={visible}
+        <CollectionCreateForm
+          wrappedComponentRef={this.saveFormRef}
           width={600}
-          onOk={this.handleOk}
+          visible={this.state.visible}
+          onCreate={this.handleCreate}
           onCancel={this.handleCancel}
-          footer={[
-            <Button key="submit" type="primary" htmlType="submit" loading={loading} onClick={this.handleOk}>
-                Submit
-            </Button>,
-            <Button key="back" onClick={this.handleCancel}>
-                Close
-            </Button>,
-          ]}
-          closable={false}
-        >
-          <h1 className='fh_rec_det_apply_title'>Position Title</h1>
-          <hr />
-          <h1 className='fh_rec_det_apply_city'>City: area</h1>
-
-          <Form layout={formLayout} onSubmit={this.handleSubmit} >
-            <div className=''>
-
-              <div className='fh_rec_det_apply_name'>
-                <Form.Item label='Name' >
-                  {
-                    getFieldDecorator('firstname', {
-                      rules: [{ required: true, message: 'Please input your first name!', whitespace: true }],
-                    })
-                    (<Input placeholder='First Name' allowClear={true} style={{width:'250px'}}/>)
-                  }
-                </Form.Item>
-                
-                <Form.Item label='' >
-                  {
-                    getFieldDecorator('lastname', {
-                    rules: [{ required: true, message: 'Please input your last name!', whitespace: true }],
-                    })
-                    (
-                      <div className='fh_rec_det_apply_last_name'>
-                        <Input placeholder='Last Name' allowClear={true} style={{width:'250px'}}/>
-                      </div>
-                    )
-                  }
-                </Form.Item>
-              </div>
-
-              <Form.Item label="E-mail" >
-                {getFieldDecorator('email', {
-                  rules: [
-                    {
-                      type: 'email',
-                      message: 'The input is not valid E-mail!',
-                    },
-                    {
-                      required: true,
-                      message: 'Please input your E-mail!',
-                    },
-                  ],
-                })(<Input placeholder='Email Address' allowClear={true} />)}
-              </Form.Item>
-
-              <Form.Item label='Description' >
-                <TextArea rows={4} allowClear={true} />
-              </Form.Item>
-
-              <Form.Item label='Upload Resume' required>
-                {/* {
-                  getFieldDecorator('resume', {
-                    rules: [ {validator: this.handleUploadValidator}],
-                  })
-                  ( */}
-                    <UploadButton title='Add a File' />
-                  {/* ) 
-                } */}
-              </Form.Item>
-
-              <hr />
-
-              <Form.Item >
-                <div className='fh_rec_det_apply_submit'>
-                  <Button type="primary" htmlType="submit" className='fh_rec_det_apply_submit_button'>
-                    Submit
-                  </Button>
-                  <Button key="back" onClick={this.handleCancel}>
-                        Close
-                  </Button>
-                </div>
-              </Form.Item>
-            </div>
-          </Form>
-
-        </Modal>
+          title={pos_title}
+          area={pos_area}
+        />
       </div>
     );
   }
--- src/component/universal_module/universal_module.jsx
+++ src/component/universal_module/universal_module.jsx
@@ -2,13 +2,15 @@ import React from 'react';
 
 const UniversalModule = (props) => {
     let ItemComponent = props.ItemComponent;
+    let pos_title = props.title;
+    let pos_area = props.area;
     return(
         <div>
-            <div className='fh_rec_det_title'>{props.title}</div>
-            <div className='fh_rec_det_area'>{props.area}</div>
+            <div className='fh_rec_det_title'>{pos_title}</div>
+            <div className='fh_rec_det_area'>{pos_area}</div>
             <div className='fh_rec_det_content'>{props.content}</div>
             <div style={{'padding-bottom':'63px'}}></div>
-            <ItemComponent />
+            <ItemComponent title={pos_title} area={pos_area}/>
         </div>
     )
     }
